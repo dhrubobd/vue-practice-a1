4dda8f1bf2d903a4352670e3f23d8ba9000cc934
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-10-05 19:53:34 +0600

    Data is shifted to separte data file and imported

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+const person =[
+    {
+      image:'./src/assets/images/person1.png',
+      name:'Victoria Harris',
+      designation:'CEO',
+      bio:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quo, tenetur nesciunt repudiandae dicta esse ex nobis facere eveniet minima, veritatis iure aspernatur officiis saepe nulla aperiam autem corrupti assumenda.',
+    },
+    {
+      image:'./src/assets/images/person2.png',
+      name:'Mark Kobe',
+      designation:'Accountant',
+      bio:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quo, tenetur nesciunt repudiandae dicta esse ex nobis facere eveniet minima, veritatis iure aspernatur officiis saepe nulla aperiam autem corrupti assumenda.',
+    },
+    {
+      image:'./src/assets/images/person3.png',
+      name:'Tannisa Chan',
+      designation:'Office Executive',
+      bio:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quo, tenetur nesciunt repudiandae dicta esse ex nobis facere eveniet minima, veritatis iure aspernatur officiis saepe nulla aperiam autem corrupti assumenda.',
+    }
+  ];
+
+  export {person}
