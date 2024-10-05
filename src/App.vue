c04e8a5155740949b7686255a10db28bcdf5a62c
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-10-05 17:39:54 +0600

    Template is ready for Data Binding Work

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,74 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <section class="grid grid-cols-3 gap-3">
+    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+            <img class="rounded-t-lg" src="./assets/images/person1.png" alt="" />
+        </a>
+        <div class="p-5 text-left">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Victoria Harris</h5>
+            </a>
+            <p class="mb-3 font-medium text-xl text-gray-700 dark:text-gray-400">CEO</p>
+            <p class="mb-3 font-normal">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quo, tenetur nesciunt repudiandae dicta esse ex nobis facere eveniet minima, veritatis iure aspernatur officiis saepe nulla aperiam autem corrupti assumenda.
+            </p>
+            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Know More
+                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+    </div>
+    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+            <img class="rounded-t-lg" src="./assets/images/person2.png" alt="" />
+        </a>
+        <div class="p-5 text-left">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mark Kobe</h5>
+            </a>
+            <p class="mb-3 font-medium text-xl text-gray-700 dark:text-gray-400">Accountant</p>
+            <p class="mb-3 font-normal">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quo, tenetur nesciunt repudiandae dicta esse ex nobis facere eveniet minima, veritatis iure aspernatur officiis saepe nulla aperiam autem corrupti assumenda.
+            </p>
+            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Know More
+                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+    </div>
+    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+            <img class="rounded-t-lg" src="./assets/images/person3.png" alt="" />
+        </a>
+        <div class="p-5 text-left">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tannisa Chan</h5>
+            </a>
+            <p class="mb-3 font-medium text-xl text-gray-700 dark:text-gray-400">Office Executive</p>
+            <p class="mb-3 font-normal">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quo, tenetur nesciunt repudiandae dicta esse ex nobis facere eveniet minima, veritatis iure aspernatur officiis saepe nulla aperiam autem corrupti assumenda.
+            </p>
+            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Know More
+                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+    </div>
+  </section>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
